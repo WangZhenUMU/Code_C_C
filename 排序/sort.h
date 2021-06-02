46d18c7f3d662f1a601d8f85366f8d91ffbc9cf9
王振 <81574995+WangZhenUMU@users.noreply.github.com>
2021-06-02 22:18:56 +0800

    Delete sort.h

--- 排序/sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SORT_H__
-#define __SORT_H__
-void InsertSort(int array[], int size);
-void ShellSort(int array[], int size);
-void SelectSort(int array[], int size);
-void SelectSortOP(int array[], int size);
-void HeapSort(int array[], int size);
-void BubbleSort(int array[], int size);
-#endif __SORT_H__
